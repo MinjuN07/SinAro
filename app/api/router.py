49cf3db462482ef06b9469997897580f63d4851e
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2024-11-21 12:10:17 +0900

    Style: 사용하지 않는 코드 삭제

--- app/api/router.py
+++ app/api/router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-from app.api.endpoints import sentiment, model, summary, letter
+from app.api.endpoints import sentiment, summary, letter
 
 api_router = APIRouter()
-api_router.include_router(model.router, prefix="/model", tags=["model"])
 api_router.include_router(sentiment.router, prefix="/analyze", tags=["analysis"])
 api_router.include_router(summary.router, prefix="/summarize", tags=["summary"])
 api_router.include_router(letter.router, prefix="/generate", tags=["generate"])
