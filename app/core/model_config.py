fae8962aad2ff5216cae602a604fb07673aa27a8
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2024-11-26 12:10:01 +0900

    Fix: 잘못된 model_type 설정 해결

--- app/core/model_config.py
+++ app/core/model_config.py
@@ -1,40 +1,13 @@
 from enum import Enum
 
 class ModelType(Enum):
-    SENTIMENT = "LETTER"
-    LETTER = "SENTIMENT"
+    SENTIMENT = "SENTIMENT"
+    LETTER = "LETTER"
     SUMMARY = "ko-llama-diary-summarizer"
 
 MODEL_NAME = {
     ModelType.SENTIMENT: "ko-gemma-9b",
     ModelType.LETTER: "ko-gemma-9b"
-}
-    
-MODEL_OPTIONS = {
-    ModelType.SENTIMENT: {
-        "num_ctx": 8096,
-        "temperature": 0.2,
-        "top_p": 0.3, 
-        "top_k": 20,
-        "num_predict": 1024,
-        "repeat_penalty": 1.5,
-        "presence_penalty": 0.5,
-        "frequency_penalty": 0.3,
-        "mirostat": 1,
-        "mirostat_tau": 0.5,
-        "stop": ["\n\n\n","\n\n","\n"]
-    },
-    ModelType.LETTER: {
-        "num_ctx": 8192,   
-        "temperature": 0.6,     
-        "top_p": 0.85,          
-        "top_k": 40,            
-        "num_predict": 4096,   
-        "frequency_penalty": 0.3,
-        "presence_penalty": 0.3,  
-        "repeat_penalty": 1.1,    
-        "stop": ["\n\n\n", "###"] 
-    },
 }
 
 MODEL_SYSTEM = {
@@ -65,4 +38,31 @@ keyword는 일기에서 가장 핵심적인 단어중 하나로 일반 명사나
 4. 반드시 하나의 완성된 편지로 만들어줘.
 5. "만약" 감정들과 키워드들 중 원본 편지와 자연스럽게 어울리지 않는 감정과 키워드가 있다면 해당 감정과 키워드는 포함하지마.
     """
+}
+
+MODEL_OPTIONS = {
+    ModelType.SENTIMENT: {
+        "num_ctx": 8096,
+        "temperature": 0.2,
+        "top_p": 0.3, 
+        "top_k": 20,
+        "num_predict": 1024,
+        "repeat_penalty": 1.5,
+        "presence_penalty": 0.5,
+        "frequency_penalty": 0.3,
+        "mirostat": 1,
+        "mirostat_tau": 0.5,
+        "stop": ["\n\n\n","\n\n","\n"]
+    },
+    ModelType.LETTER: {
+        "num_ctx": 8192,   
+        "temperature": 0.6,     
+        "top_p": 0.85,          
+        "top_k": 40,            
+        "num_predict": 4096,   
+        "frequency_penalty": 0.3,
+        "presence_penalty": 0.3,  
+        "repeat_penalty": 1.1,    
+        "stop": ["\n\n\n", "###"] 
+    },
 }
